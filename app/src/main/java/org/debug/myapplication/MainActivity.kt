9b12ccc7dbef0b249e7d370781102feec3dfb604
Tolriq <tolriq@tolriq.com>
2020-04-30 11:17:07 +0200

    Repro of issue see TODO in mylibrary/build.gradle

---
+++ app/src/main/java/org/debug/myapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.debug.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
